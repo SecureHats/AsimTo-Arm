bf4b45e51a1abc26b79717ffebc5889d3e63ce71
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2023-08-21 14:12:57 +0200

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -1,14 +1,38 @@
 <#
-    Title:          Template for GitHub Action
+    Title:          AsimTo-ARM Converter
     Language:       PowerShell
     Version:        1.0
     Author:         Rogier Dijkman
-    Last Modified:  04/07/2023
+    Last Modified:  21/08/2023
 
     DESCRIPTION
-    This GitHub action is used to ...
-
+    This GitHub action is used to convert ASIM parser files to deployable ARM templates.
 #>
 
 param (
+    [Parameter(Mandatory = $true)]
+    [string]$FilesPath = '.',
+
+    [Parameter(Mandatory = $true)]
+    [string]$OutputPath = '.',
+
+    [Parameter(Mandatory = $false)]
+    [string]$returnObject = 'false'
+        
 )
+
+try {
+    Write-Verbose "Importing Helper Module"
+    Import-Module "$($PSScriptRoot)/modules/HelperFunctions.psm1"
+} catch {
+    Write-Error $_.Exception.Message
+    break
+}
+
+$hashTable = @{
+    FilesPath    = $FilesPath
+    OutputPath   = $OutputPath
+    ReturnObject = [System.Convert]::ToBoolean($ReturnObject)
+}
+
+Convert-AsimToArm @hashTable
